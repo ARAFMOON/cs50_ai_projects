4f7e71a0a8123cb5fff95a9db528f7d0c69f3cdd
ARAFMOON <kamalat1979@gmail.com>
2024-10-22 01:11:56 +0600

    Update _init_.py

---
+++ _init_.py
@@ -0,0 +1,7 @@
+# __init__.py
+
+# Import necessary modules or functions
+from .tictactoe import initial_state, player, actions, result, winner, terminal, minimax
+
+# Package-level metadata
+__version__ = "1.0.0"
